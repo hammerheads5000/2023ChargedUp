270b32dfaf77a29d0355b528459907964c6fb068
NoahDaNerd <ndtech260@gmail.com>
2023-03-09 14:47:40 -0500

    test path

---
+++ src/main/java/frc/robot/autos/TestPathFollow.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autos;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.AutoPathFollowCommand;
+import frc.robot.subsystems.Swerve;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class TestPathFollow extends SequentialCommandGroup {
+  /** Creates a new TestPathFollow. */
+  public TestPathFollow(Swerve s_swerve) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    Pose2d[] poses = new Pose2d[]{ new Pose2d(0, 0, new Rotation2d(0)), new Pose2d(1, 0, new Rotation2d(0))};
+    addCommands(
+        new AutoPathFollowCommand(poses, s_swerve)
+    );
+  }
+}
